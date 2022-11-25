e7e89c90ea8bed1dd248314cd527ab58931eb1cf
Davi Liporace <davimarquesr@gmail.com>
2022-11-25 14:42:38 -0300

    Post and get Cart

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,5 @@
 import Router from "express";
 import { signIn, signUp } from "../controllers/auth.controllers.js";
-import { GetStock, Stockin } from "../controllers/stock.controllers.js";
 import {
   userValidation,
   signInValidation,
@@ -11,7 +10,5 @@ const router = Router();
 //A página stock.routes está dando erro então estou usando as rotas aqui por enquanto para não ficar muito travado no projeto.
 router.post("/signup", userValidation, signUp);
 router.post("/signin", signInValidation, signIn);
-router.post("/Stockin", Stockin);
-router.get("/Stockin", GetStock);
 
 export default router;
