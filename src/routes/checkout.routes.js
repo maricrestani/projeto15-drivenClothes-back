28c98ae84e8c5129ad4f6b6ab16dbdf918c225fa
mcrestani <marianascrestani@gmail.com>
2022-11-25 19:04:20 -0300

    feat: checkout route

--- src/routes/checkout.routes.js
+++ src/routes/checkout.routes.js
@@ -1,10 +1,19 @@
 import Router from "express";
-import { CloseOrder} from "../controllers/checkout.controllers.js";
+//import { CloseOrder} from "../controllers/checkout.controllers.js";
 
 const router = Router();
 
 //get dos produtos do cartCollection =  const { img, description, serial, value } = req.body;
 //post dos produtos selecionado + adress + card na checkoutCollection//
-router.post("/checkout", CloseOrder)
+//router.post("/checkout", CloseOrder)
 
 export default router;
+
+/*
+- e-mail vindo do signin
+- valor da compra
+-preencher dado do cartão
+-Comprar
+-delete no carrinho
+-delete no serial
+*/
