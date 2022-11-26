825627b20c126d42269a264f145307a07959981a
Davi Liporace <davimarquesr@gmail.com>
2022-11-26 14:33:09 -0300

    Add cart delete button

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,6 +1,7 @@
 import { cartCollection, stockCollection } from "../database/db.js";
 import bcrypt from "bcrypt";
 import { v4 as uuidV4 } from "uuid";
+import { ObjectId } from "mongodb";
 
 export async function AddCart(req, res) {
   const { img, description, serial, value } = req.body;
@@ -21,5 +22,26 @@ export async function GetCart(req, res) {
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
+  }
+}
+
+/* export async function DeleteItem(req, res) {
+  const id = req.body;
+  console.log(id);
+  try {
+    await cartCollection.deleteOne({ _id: ObjectId(id) });
+    res.send(id);
+  } catch (err) {
+    res.send(err);
+  }
+} */
+export async function DeleteItem(req, res) {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    await cartCollection.deleteOne({ _id: ObjectId(id) });
+    res.send(id);
+  } catch (err) {
+    res.send(err);
   }
 }
