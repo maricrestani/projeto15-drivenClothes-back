88fc9d5faca6c22664ca805711d38337782281a1
Davi Liporace <davimarquesr@gmail.com>
2022-11-27 19:15:23 -0300

    created cart routes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ dotenv.config();
 import authroutes from "./routes/auth.routes.js";
 import stockroutes from "./routes/stock.routes.js";
 import checkoutroutes from "./routes/checkout.routes.js";
+import cartroutes from "./routes/cart.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(authroutes, stockroutes, checkoutroutes);
+app.use(authroutes, stockroutes, checkoutroutes, cartroutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -1,16 +1,10 @@
 import Router from "express";
-import {
-  AddCart,
-  DeleteItem,
-  GetCart,
-} from "../controllers/cart.controllers.js";
+
 import { GetStock, Stockin } from "../controllers/stock.controllers.js";
 
 const router = Router();
 
 router.post("/Stockin", Stockin);
 router.get("/Stockin", GetStock);
-router.post("/cart", AddCart);
-router.get("/cart", GetCart);
-router.delete("/cart/:id", DeleteItem);
+
 export default router;
